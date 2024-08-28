3490899f795fa5dc4e93f44739cc9c8e693543c3
dVeon-loch <39005321+dVeon-loch@users.noreply.github.com>
2024-08-28 10:06:36 +0200

    Example project updates - Add function stubs and modules for example project. - Change default generation dir for example project - Update README

--- examples/minimal_project/gen/build.rs
+++ examples/minimal_project/gen/build.rs
@@ -2,20 +2,23 @@ use std::{env, path::PathBuf};
 
 use endpoint_gen::Data;
 
+mod gen_src;
+
 fn main() -> eyre::Result<()> {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=../docs/error_codes/error_codes.json");
 
     // Set these up to match your environment
-    let root = env::current_dir()?; // This should evaluate to the root of your project where the project Cargo.toml can be found
-    let output_dir = env::current_dir()?; // This should evaluate to the `<root>/gen/` dir
+    let current_dir = env::current_dir()?;
+    let root = current_dir.parent().unwrap(); // This should evaluate to the root of your project where the project Cargo.toml can be found
+    let output_dir = &current_dir.join("generated"); // This should evaluate to the `<root>/gen/generated/` dir
 
     let data = Data {
         project_root: PathBuf::from(&root),
-        output_dir: PathBuf::from(root),
-        services: services::get_services(),
-        enums: enums::get_enums(),
-        pg_funcs: proc_funcs::get_proc_functions(),
+        output_dir: PathBuf::from(output_dir),
+        services: gen_src::services::get_services(),
+        enums: gen_src::enums::get_enums(),
+        pg_funcs: gen_src::proc_funcs::get_proc_functions(),
     };
 
     endpoint_gen::main(data)?;
