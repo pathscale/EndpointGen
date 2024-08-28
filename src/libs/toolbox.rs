977bdea73421e760cabc6f85b352a175d2ae2b34
dVeon-loch <39005321+dVeon-loch@users.noreply.github.com>
2024-08-28 15:19:16 +0200

    Libs bundling refactor Pt1 - Add all libs that we currently bundle into our projects as modules for endpoint-gen - Add some of the dependencies that are required by these modules (in-progress) - TODO: Need to go through all of these to prune out what isn't used by the files that endpoint-gen generates

---
+++ src/libs/toolbox.rs
@@ -0,0 +1,212 @@
+use dashmap::DashMap;
+use eyre::*;
+use serde::*;
+use serde_json::Value;
+use std::fmt::{Debug, Display, Formatter};
+use std::net::IpAddr;
+use std::result::Result::Ok;
+use std::sync::atomic::Ordering;
+use std::sync::{Arc, RwLock};
+use tokio_tungstenite::tungstenite::Message;
+use tracing::*;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct NoResponseError;
+
+impl Display for NoResponseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str("NoResp")
+    }
+}
+
+impl std::error::Error for NoResponseError {}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct CustomError {
+    pub code: ErrorCode,
+    pub params: Value,
+}
+
+impl CustomError {
+    pub fn new(code: impl Into<ErrorCode>, reason: impl Serialize) -> Self {
+        Self {
+            code: code.into(),
+            params: serde_json::to_value(reason)
+                .context("Failed to serialize error reason")
+                .unwrap(),
+        }
+    }
+    pub fn from_sql_error(err: &str, msg: impl Display) -> Result<Self> {
+        let code = u32::from_str_radix(err, 36)?;
+        let error_code = ErrorCode::new(code);
+        let this = Self {
+            code: error_code,
+            params: msg.to_string().into(),
+        };
+
+        Ok(this)
+    }
+}
+
+impl Display for CustomError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.params.to_string())
+    }
+}
+
+impl std::error::Error for CustomError {}
+
+#[derive(Copy, Clone)]
+pub struct RequestContext {
+    pub connection_id: ConnectionId,
+    pub user_id: i64,
+    pub seq: u32,
+    pub method: u32,
+    pub log_id: u64,
+    pub role: u32,
+    pub ip_addr: IpAddr,
+}
+impl RequestContext {
+    pub fn from_conn(conn: &WsConnection) -> Self {
+        Self {
+            connection_id: conn.connection_id,
+            user_id: conn.get_user_id(),
+            seq: 0,
+            method: 0,
+            log_id: conn.log_id,
+            role: conn.role.load(Ordering::Relaxed),
+            ip_addr: conn.address.ip(),
+        }
+    }
+}
+// TODO: make it shared and cloneable
+
+pub struct Toolbox {
+    pub send_msg: RwLock<Arc<dyn Fn(ConnectionId, WsResponseValue) -> bool + Send + Sync>>,
+}
+pub type ArcToolbox = Arc<Toolbox>;
+impl Toolbox {
+    pub fn new() -> Arc<Self> {
+        Arc::new(Self {
+            send_msg: RwLock::new(Arc::new(|_conn_id, _msg| false)),
+        })
+    }
+
+    pub fn set_ws_states(
+        &self,
+        states: Arc<DashMap<ConnectionId, Arc<WsStreamState>>>,
+        oneshot: bool,
+    ) {
+        *self.send_msg.write().unwrap() = Arc::new(move |conn_id, msg| {
+            let state = if let Some(state) = states.get(&conn_id) {
+                state
+            } else {
+                return false;
+            };
+            Self::send_ws_msg(&state.message_queue, msg, oneshot);
+            true
+        });
+    }
+
+    pub fn send_ws_msg(
+        sender: &tokio::sync::mpsc::Sender<Message>,
+        resp: WsResponseValue,
+        oneshot: bool,
+    ) {
+        let resp = serde_json::to_string(&resp).unwrap();
+        if let Err(err) = sender.try_send(resp.into()) {
+            warn!("Failed to send websocket message: {:?}", err)
+        }
+        if oneshot {
+            let _ = sender.try_send(Message::Close(None));
+        }
+    }
+    pub fn send(&self, conn_id: ConnectionId, resp: WsResponseValue) -> bool {
+        (self.send_msg.read().unwrap())(conn_id, resp)
+    }
+    pub fn send_response(&self, ctx: &RequestContext, resp: impl Serialize) {
+        self.send(
+            ctx.connection_id,
+            WsResponseValue::Immediate(WsSuccessResponse {
+                method: ctx.method,
+                seq: ctx.seq,
+                params: serde_json::to_value(&resp).unwrap(),
+            }),
+        );
+    }
+    pub fn send_internal_error(&self, ctx: &RequestContext, code: ErrorCode, err: Error) {
+        self.send(ctx.connection_id, internal_error_to_resp(ctx, code, err));
+    }
+    pub fn send_request_error(&self, ctx: &RequestContext, code: ErrorCode, err: impl Into<Value>) {
+        self.send(ctx.connection_id, request_error_to_resp(ctx, code, err));
+    }
+    pub fn send_log(&self, ctx: &RequestContext, level: LogLevel, msg: impl Into<String>) {
+        self.send(
+            ctx.connection_id,
+            WsResponseValue::Log(WsLogResponse {
+                seq: ctx.seq,
+                log_id: ctx.log_id,
+                level,
+                message: msg.into(),
+            }),
+        );
+    }
+    pub fn encode_ws_response<Resp: Serialize>(
+        ctx: RequestContext,
+        resp: Result<Resp>,
+    ) -> Option<WsResponseValue> {
+        #[allow(unused_variables)]
+        let RequestContext {
+            connection_id,
+            user_id,
+            seq,
+            method,
+            log_id,
+            ..
+        } = ctx;
+        let resp = match resp {
+            Ok(ok) => WsResponseValue::Immediate(WsSuccessResponse {
+                method,
+                seq,
+                params: serde_json::to_value(ok).expect("Failed to serialize response"),
+            }),
+            Err(err) if err.is::<NoResponseError>() => {
+                return None;
+            }
+
+            Err(err0) if err0.is::<tokio_postgres::Error>() => {
+                error!("Database error: {:?}", err0);
+
+                let err = err0.downcast_ref::<tokio_postgres::Error>().unwrap();
+                if let Some(code) = err.code() {
+                    if let Ok(err) = CustomError::from_sql_error(code.code(), &err0) {
+                        request_error_to_resp(&ctx, err.code, err.params)
+                    } else {
+                        internal_error_to_resp(
+                            &ctx,
+                            ErrorCode::new(100601), // Database Error,
+                            err0,
+                        )
+                    }
+                } else {
+                    internal_error_to_resp(
+                        &ctx,
+                        ErrorCode::new(100601), // Database Error,
+                        err0,
+                    )
+                }
+            }
+            Err(err) if err.is::<CustomError>() => {
+                error!("CustomError: {:?}", err);
+                let err = err.downcast::<CustomError>().unwrap();
+                request_error_to_resp(&ctx, err.code, err.params)
+            }
+            Err(err) => internal_error_to_resp(
+                &ctx,
+                ErrorCode::new(100500), // Internal Error
+                err,
+            ),
+        };
+        Some(resp)
+    }
+}
---
+++ src/libs/ws/mod.rs
@@ -0,0 +1,15 @@
+mod basics;
+mod client;
+mod conn;
+mod headers;
+mod push;
+mod server;
+mod session;
+
+pub use basics::*;
+pub use client::*;
+pub use conn::*;
+pub use headers::*;
+pub use push::*;
+pub use server::*;
+pub use session::*;
