fa308db2ed2aea12051648e9bc2192b8800da6d1
dVeon-loch <39005321+dVeon-loch@users.noreply.github.com>
2024-08-28 08:59:53 +0200

    Add an example project

---
+++ examples/minimal_project/gen/src/proc_funcs.rs
@@ -0,0 +1,6 @@
+use endpoint_gen::model::ProceduralFunction;
+
+/// Returns a vector of the available `ProceduralFunction`s (e.g. `auth`, `user`, `admin`, `chatbot`).
+pub fn get_proc_functions() -> Vec<ProceduralFunction> {
+    vec![]
+}
