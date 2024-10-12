198989bc41822525692225ee02e1624c3d234661
dVeon-loch <dveonloch@protonmail.com>
2024-10-12 23:20:22 +0200

    Complete refactor of file ingestion. - Still takes a directory as input, but now searches for RON "schema" object that represents the type of file that is being parsed

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::{
-    fs::{self},
-    path::PathBuf,
-    str::FromStr,
+    collections::HashMap, path::{Path, PathBuf}, str::FromStr
 };
 
 use clap::Parser;
-use endpoint_libs::model::{Service, Type};
+use endpoint_libs::model::{EndpointSchema, Service, Type};
 use eyre::*;
 use serde::Deserialize;
+use walkdir::WalkDir;
 use std::env;
 use std::result::Result::Ok;
 
@@ -39,58 +38,120 @@ pub struct Data {
     pub enums: Vec<Type>,
 }
 
-#[derive(Deserialize, Debug)]
-struct ServicesConfig {
-    pub services: Vec<Service>,
+enum RustConfig {
+    Service(Service),
+    EndpointSchema(String, EndpointSchema),
+    EndpointSchemaList(String, Vec<EndpointSchema>),
+    Enum(Type),
+    EnumList(Vec<Type>),
 }
 
-#[derive(Deserialize, Debug)]
-struct EnumsConfig {
-    pub types: Vec<Type>,
-}
-
-enum FileType {
+#[derive(Deserialize)]
+enum SchemaType {
     Service,
     Enum,
+    EnumList,
+    EndpointSchema(String),
+    EndpointSchemaList(String),
 }
 
-enum RustConfig {
-    Service(ServicesConfig),
-    Enum(EnumsConfig),
+#[derive(Deserialize)]
+struct RonSchema {
+    schema_type: SchemaType,
 }
 
-fn process_input_files(dir: PathBuf, filetype: FileType) -> eyre::Result<Vec<RustConfig>> {
-    let path = dir.as_path();
+fn process_file(file_path: &Path) -> eyre::Result<RustConfig> {
+    match file_path.extension() {
+        Some(extension) if extension == "ron" => {
+            let file_string = ron::to_string(&file_path)?;
+            let schema: RonSchema = ron::from_str(&file_string)?;
+
+            match schema.schema_type {
+                SchemaType::Service => {
+                    let service: Service = ron::from_str(&file_string)?;
+                    return Ok(RustConfig::Service(service));
+                },
+                SchemaType::Enum => {
+                    let enum_type: Type = ron::from_str(&file_string)?;
+                    return Ok(RustConfig::Enum(enum_type));
+                },
+                SchemaType::EnumList => {
+                    let enums: Vec<Type> = ron::from_str(&file_string)?;
+                    return Ok(RustConfig::EnumList(enums));
+                },
+                SchemaType::EndpointSchema(service_name) => {
+                    let endpoint_schema: EndpointSchema = ron::from_str(&file_string)?;
+                    return Ok(RustConfig::EndpointSchema(service_name, endpoint_schema));
+                },
+                SchemaType::EndpointSchemaList(service_name) => {
+                    let endpoint_schemas: Vec<EndpointSchema> = ron::from_str(&file_string)?;
+                    return Ok(RustConfig::EndpointSchemaList(service_name, endpoint_schemas));
+                },
+            }
+        },
+        _ => Err(eyre!("Non RON file OR file without extension in config dir ")),
+    }
+}
+
+fn process_input_files(dir: PathBuf) -> eyre::Result<Vec<RustConfig>> {
+    let root = dir.as_path();
     let mut rust_configs: Vec<RustConfig> = vec![];
 
-    if path.is_dir() {
-        for entry in fs::read_dir(path)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_file() {
-                match path.extension() {
-                    Some(extension) if extension == "ron" => match filetype {
-                        FileType::Service => {
-                            let service_content = fs::read_to_string(&path)?;
-                            let services: ServicesConfig = ron::from_str(&service_content)?;
+    // Walk through the directory and all subdirectories
+    for entry in WalkDir::new(root).into_iter().filter_map(Result::ok) {
+        let path = entry.path();
 
-                            rust_configs.push(RustConfig::Service(services));
-                        }
-                        FileType::Enum => {
-                            let enums_content = fs::read_to_string(&path)?;
-                            let enums: EnumsConfig = ron::from_str(&enums_content)?;
-
-                            rust_configs.push(RustConfig::Enum(enums));
-                        }
-                    },
-                    _ => continue,
-                }
+        if path.is_file() {
+            match process_file(path) {
+                Ok(rust_config) => rust_configs.push(rust_config),
+                Err(err) => eprintln!("Error processing file: {path:?}, Error: {err}"),
             }
         }
-    } else {
-        return Err(eyre!("The provided path is not a directory."));
     }
+
     Ok(rust_configs)
+}
+
+struct InputObjects {
+    services: Vec<Service>,
+    enums: Vec<Type>,
+} 
+
+fn build_object_lists(dir: PathBuf) -> eyre::Result<InputObjects> {
+    let rust_configs = process_input_files(dir)?;
+
+    let mut service_schema_map: HashMap<String, Vec<EndpointSchema>> = HashMap::new();
+
+    let mut services: Vec<Service> = vec![];
+
+    let mut enums: Vec<Type> = vec![];
+
+    for config in rust_configs {
+        match config {
+            RustConfig::Service(service) => services.push(service),
+            RustConfig::EndpointSchema(service_name, endpoint_schema) => {
+                service_schema_map.entry(service_name).or_insert(vec![]).push(endpoint_schema)
+            },
+            RustConfig::EndpointSchemaList(service_name, endpoint_schemas) => {
+                service_schema_map.entry(service_name).or_insert(vec![]).extend(endpoint_schemas)
+            },
+            RustConfig::Enum(enum_type) => enums.push(enum_type),
+            RustConfig::EnumList(enum_types) => enums.extend(enum_types),
+        }
+    }
+
+    let mut service_num = 1;
+    if !service_schema_map.is_empty() {
+        for (service_name, endpoint_schemas) in service_schema_map {
+            services.push(Service::new(service_name, service_num.clone(), endpoint_schemas));
+            service_num += 1;
+        }
+    }
+
+    Ok(InputObjects {
+        services,
+        enums
+    })
 }
 
 fn main() -> Result<()> {
@@ -114,48 +175,13 @@ fn main() -> Result<()> {
 
     let output_dir = generation_root.join("generated");
 
-    let services = {
-        let mut services: Vec<Service> = vec![];
-
-        match process_input_files(config_dir.join("services"), FileType::Service) {
-            Ok(rust_configs) => {
-                for config in rust_configs {
-                    match config {
-                        RustConfig::Service(services_config) => {
-                            services.extend(services_config.services)
-                        }
-                        RustConfig::Enum(_) => panic!["Service input files returning enums!"],
-                    }
-                }
-            }
-            Err(err) => {
-                panic!["Error processing input service files: {err}"]
-            }
-        }
-        services
-    };
-
-    let enums = {
-        let mut enums: Vec<Type> = vec![];
-        match process_input_files(config_dir.join("enums"), FileType::Enum) {
-            Ok(rust_configs) => for config in rust_configs {
-                match config {
-                    RustConfig::Service(_) => panic!["Enum input files returning services!"],
-                    RustConfig::Enum(enums_config) => enums.extend(enums_config.types),
-                }
-            },
-            Err(err) => {
-                panic!["Error processing input enum files: {err}"]
-            }
-        }
-        enums
-    };
+    let input_objects = build_object_lists(config_dir)?;
 
     let data = Data {
         project_root: generation_root,
         output_dir,
-        services,
-        enums,
+        services: input_objects.services,
+        enums: input_objects.enums,
     };
 
     docs::gen_services_docs(&data)?;
